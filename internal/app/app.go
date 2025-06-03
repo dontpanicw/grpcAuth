1719de2cef974fe1878fc623f4de9fe63def93e0
dontpanicw <valerycecet8@gmail.com>
2025-06-03 20:17:21 +0300

    ...

---
+++ internal/app/app.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	"google.golang.org/grpc"
+	"log/slog"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       string
+}
+
+func New(
+	log *slog.Logger,
+	port string,
+) *App {
+	gRPCServer := grpc.NewServer()
+}
